1ff7c33eb187e672fcb2fb5ab5bb61c166c72055
Ahmet Cengiz <55302286+ahfercen@users.noreply.github.com>
2021-12-08 22:19:01 -0500

    Add files via upload

---
+++ Image.py
@@ -0,0 +1,6 @@
+class Image:
+	def __init__(self, img):
+		self.original = cv.normalize(src=img, dst=None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX, dtype=cv.CV_8U)
+		self.blurred = []
+		self.boxes = []
+		self.trueBox = []
