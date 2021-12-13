77b672592ee7b1152c256cc4b786167615c6e813
Ahmet Cengiz <55302286+ahfercen@users.noreply.github.com>
2021-12-12 19:01:53 -0500

    Update DNN.py

--- DNN.py
+++ DNN.py
@@ -14,6 +14,8 @@ configFile = "models/deploy.prototxt"
 def blur(img, x, y, w, h):
 	updated = img.copy()
 	blurred = cv.GaussianBlur(updated, (75, 75), 0)
+	blurred = cv.GaussianBlur(blurred, (75, 75), 0)
+	blurred = cv.GaussianBlur(blurred, (75, 75), 0)
 	updated[y:h, x:w] = blurred[y:h, x:w]
 	return updated
 
@@ -37,29 +39,10 @@ class DNN(Model):
 			self.model.setInput(im_blob)
 			img.boxes = self.model.forward()
 
-			if sample_result:
-				# Show original image with true box
-				ox = img.true_boxes[0][0]
-				oy = img.true_boxes[0][1]
-				ox1 = img.true_boxes[0][2]
-				oy1 = img.true_boxes[0][3]
-				cv.imshow('Original', img.original)
-				cv.waitKey(0)
-				og = cv.rectangle(img.original, (ox,oy),(ox+ox1,oy+oy1),(0,0,255),3)
-				# cv.imshow('Original', og)
-				# cv.waitKey(0)
-				pr = img.original
-				for detected_box in img.boxes:
-					# Show image with predicted bounding box
-					(x, y, x1, y1) = detected_box.astype("int")
-					pr = cv.rectangle(img.original, (x,y),(x+x1,y+y1),(0,255,0),3)
-				cv.imshow('Predicted', pr)
-				cv.waitKey(0)
-				# Show image after blurring
-				cv.imshow('Blurred', img.blurred)
-				cv.waitKey(0)
+			
 
 	def draw_faces(self):
+		sample_result = True    # Show the model result for 2 images
 		for im in self.images:
 			h, w = im.original.shape[:2]
 			confidence = im.boxes[0, 0, 0, 2]
@@ -67,6 +50,27 @@ class DNN(Model):
 				box = im.boxes[0, 0, 0, 3:7] * np.array([w, h, w, h])
 				(x, y, x1, y1) = box.astype("int")
 				im.blurred = blur(im.original, x, y, x1, y1)
+				if sample_result:
+					# Show original image with true box
+					ox = im.true_boxes[0][0]
+					oy = im.true_boxes[0][1]
+					ox1 = im.true_boxes[0][2]
+					oy1 = im.true_boxes[0][3]
+					cv.imshow('Original', im.original)
+					cv.waitKey(0)
+					og = cv.rectangle(im.original, (ox,oy),(ox+ox1,oy+oy1),(0,0,255),3)
+					pr = im.original
+					for detected_box in im.boxes:
+						# Show image with predicted bounding box
+						h, w = im.original.shape[:2]
+						bb = detected_box[0, 0, 3:7] * np.array([w,h,w,h])
+						(x, y, x1, y1) = bb.astype("int")
+						pr = cv.rectangle(im.original, (x,y),(x1,y1),(0,255,0),3)
+					cv.imshow('Predicted', pr)
+					cv.waitKey(0)
+					#Show image after blurring
+					cv.imshow('Blurred', im.blurred)
+					cv.waitKey(0)
 				# show = cv.rectangle(im.blurred, (x, y),(x1, y1), (0, 255, 0), 3)
 				# a,b,c,d = im.true_boxes[0]
 				# show = cv.rectangle(im.blurred, (a,b),(a+c,b+d),(255,0,0),3)
@@ -74,4 +78,3 @@ class DNN(Model):
 				# print(im.true_boxes)
 				# cv.imshow('lines',show)
 				# cv.waitKey(0)
-
